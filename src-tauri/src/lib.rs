efeb1b18d27253234d3c98eac553785a9b701d52
Hahihula <hahihula@gmail.com>
2024-10-08 13:33:03 +0200

    Eim-42 loading config file from file (#4)
    * added file loading dialog
    
    * loading setting from file
    
    ---------
    
    Co-authored-by: Petr Gadorek <petr.gadorek@espressif.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,12 @@
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
+use std::fs::File;
+use std::io::Read;
 
 #[derive(Default, Serialize, Deserialize)]
 pub struct AppState {
     wizard_data: Mutex<WizardData>,
+    settings: Mutex<idf_im_lib::settings::Settings>,
 }
 
 #[derive(Default, Serialize, Deserialize)]
@@ -101,32 +104,45 @@ fn python_install() -> bool {
 }
 #[tauri::command]
 async fn get_available_targets() -> Vec<String> {
-    let mut available_targets = idf_im_lib::idf_versions::get_avalible_targets().await.unwrap();
+    let mut available_targets = idf_im_lib::idf_versions::get_avalible_targets()
+        .await
+        .unwrap();
     available_targets.insert(0, "all".to_string());
     available_targets
 }
 
 #[tauri::command]
 async fn get_idf_versions() -> Vec<String> {
-  let target = "all".to_string(); //todo: get from state or user
-  let mut available_versions = if target == "all" {
-      //todo process vector of targets
-      idf_im_lib::idf_versions::get_idf_names().await
-  } else {
-      idf_im_lib::idf_versions::get_idf_name_by_target(&target.to_string().to_lowercase()).await
-  };
-  available_versions.push("master".to_string());
-  available_versions
+    let target = "all".to_string(); //todo: get from state or user
+    let mut available_versions = if target == "all" {
+        //todo process vector of targets
+        idf_im_lib::idf_versions::get_idf_names().await
+    } else {
+        idf_im_lib::idf_versions::get_idf_name_by_target(&target.to_string().to_lowercase()).await
+    };
+    available_versions.push("master".to_string());
+    available_versions
 }
 
 #[tauri::command]
 fn get_idf_mirror_list() -> &'static [&'static str] {
-  idf_im_lib::get_idf_mirrors_list()
+    idf_im_lib::get_idf_mirrors_list()
 }
 
 #[tauri::command]
 fn get_tools_mirror_list() -> &'static [&'static str] {
-  idf_im_lib::get_idf_tools_mirrors_list()
+    idf_im_lib::get_idf_tools_mirrors_list()
+}
+
+#[tauri::command]
+fn load_settings(path: &str) -> idf_im_lib::settings::Settings { //TODO emit errors to FE
+  let mut file = File::open(path).expect("Failed to open file");
+  let mut contents = String::new();
+  file.read_to_string(&mut contents).expect("Failed to read file");
+  let settings: idf_im_lib::settings::Settings = toml::from_str(&contents).expect("Failed to parse config file");
+  println!("settings {:?}", settings); // TODO: remove debug print statement
+  // TODO: load setting to the app state
+  settings
 }
 
 use tauri::Manager;
@@ -134,6 +150,7 @@ use tauri::Manager;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             let app_state = AppState::default();
             app.manage(app_state);
@@ -152,7 +169,8 @@ pub fn run() {
             get_available_targets,
             get_idf_versions,
             get_idf_mirror_list,
-            get_tools_mirror_list
+            get_tools_mirror_list,
+            load_settings
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/components/LoadConfig.vue
+++ src/components/LoadConfig.vue
@@ -5,7 +5,7 @@
       <template #1>
         <div :style="{ height: '200px' }">
           ( TODO: put opemn config icon here )
-          <n-button type="primary" ghost>
+          <n-button @click="load_config" type="primary" ghost>
             Load instalation config.
           </n-button>
         </div>
@@ -24,7 +24,9 @@
 </template>
 
 <script>
+import { open } from '@tauri-apps/plugin-dialog';
 import { NSplit, NButton } from 'naive-ui'
+import { invoke } from "@tauri-apps/api/core";
 
 export default {
   name: 'LoadConfig',
@@ -32,6 +34,20 @@ export default {
   methods: {
     startWizard() {
       this.$router.push('/wizard/1');
+    },
+    async load_config() {
+      console.log('Loading config...');
+      const file = await open({
+        title: 'Select installation config file',
+        multiple: false,
+        directory: false,
+        filters: [
+          { name: '*', extensions: ['toml'] },
+        ],
+      });
+      console.log(file);
+      const content = await invoke("load_settings", { path: file });
+      console.log(content);
     }
   }
 }
