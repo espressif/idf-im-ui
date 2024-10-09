26231082c15f2864726e5a9eef60a47da0511d29
Petr Gadorek <petr.gadorek@espressif.com>
2024-10-09 15:30:45 +0200

    setup proper logging

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -19,7 +19,7 @@ struct WizardData {
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
-    println!("Greet called with name: {}", name);
+    log::info!("Greet called with name: {}", name);
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
@@ -32,19 +32,19 @@ fn get_settings(app_handle: tauri::AppHandle) -> idf_im_lib::settings::Settings 
 
 #[tauri::command]
 fn get_prequisites() -> Vec<&'static str> {
-    println!("Get prerequisites called"); // todo remove debug statement
+    log::info!("Get prerequisites called"); // todo remove debug statement
     idf_im_lib::system_dependencies::get_prequisites()
 }
 
 #[tauri::command]
 fn get_operating_system() -> String {
-    println!("Get operating system called"); // todo remove debug statement
+    log::info!("Get operating system called"); // todo remove debug statement
     std::env::consts::OS.to_string()
 }
 
 #[tauri::command]
 fn install_prerequisites(app_handle: AppHandle) -> bool {
-    println!("Install prerequisites called"); // todo remove debug statement
+    log::info!("Install prerequisites called"); // todo remove debug statement
     let unsatisfied_prerequisites = idf_im_lib::system_dependencies::check_prerequisites()
         .unwrap()
         .into_iter()
@@ -58,7 +58,7 @@ fn install_prerequisites(app_handle: AppHandle) -> bool {
                 format!("Error installing prerequisites: {}", err),
                 "error".to_string(),
             );
-            eprintln!("Error installing prerequisites: {}", err);
+            log::error!("Error installing prerequisites: {}", err);
             false
         }
     }
@@ -82,7 +82,7 @@ fn check_prequisites(app_handle: AppHandle) -> Vec<String> {
                 format!("Error checking prerequisites: {}", err),
                 "error".to_string(),
             );
-            eprintln!("Error checking prerequisites: {}", err); //TODO: emit message
+            log::error!("Error checking prerequisites: {}", err); //TODO: emit message
             vec![]
         }
     }
@@ -102,7 +102,7 @@ fn python_sanity_check(app_handle: AppHandle, python: Option<&str>) -> bool {
                     format!("Python sanity check failed: {}", err),
                     "warning".to_string(),
                 );
-                println!("{:?}", err)
+                log::warn!("{:?}", err)
             }
         }
     }
@@ -119,7 +119,7 @@ fn python_install(app_handle: AppHandle) -> bool {
                 format!("Error installing python: {}", err),
                 "error".to_string(),
             );
-            eprintln!("Error installing python: {}", err); //TODO: emit message
+            log::error!("Error installing python: {}", err); //TODO: emit message
             false
         }
     }
@@ -135,7 +135,7 @@ async fn get_available_targets() -> Vec<String> {
 
 #[tauri::command]
 fn set_targets(app_handle: AppHandle, targets: Vec<String>) {
-    println!("set_targets called: {:?}", targets); //todo: switch to debug!
+    log::info!("set_targets called: {:?}", targets); //todo: switch to debug!
     let app_state = app_handle.state::<AppState>();
     let mut settings = app_state
         .settings
@@ -149,7 +149,7 @@ fn set_targets(app_handle: AppHandle, targets: Vec<String>) {
         })
         .expect("Failed to lock settings");
     (*settings).target = Some(targets);
-    println!("Setting after targets: {:?}", settings); //todo: switch to debug!
+    log::debug!("Setting after targets: {:?}", settings); //todo: switch to debug!
 }
 
 #[tauri::command]
@@ -188,7 +188,7 @@ async fn get_idf_versions(app_handle: AppHandle) -> Vec<String> {
 
 #[tauri::command]
 fn set_versions(app_handle: AppHandle, versions: Vec<String>) {
-    println!("set_versions called: {:?}", versions); //todo: switch to debug!
+    log::info!("set_versions called: {:?}", versions); //todo: switch to debug!
     let app_state = app_handle.state::<AppState>();
     let mut settings = app_state
         .settings
@@ -202,7 +202,7 @@ fn set_versions(app_handle: AppHandle, versions: Vec<String>) {
         })
         .expect("Failed to lock settings");
     (*settings).idf_versions = Some(versions);
-    println!("Setting after versions: {:?}", settings); //todo: switch to debug!
+    log::debug!("Setting after versions: {:?}", settings); //todo: switch to debug!
 }
 
 #[tauri::command]
@@ -212,7 +212,7 @@ fn get_idf_mirror_list() -> &'static [&'static str] {
 
 #[tauri::command]
 fn set_idf_mirror(app_handle: AppHandle, mirror: &str) {
-    println!("set_idf_mirror called: {}", mirror); //todo: switch to debug!
+    log::info!("set_idf_mirror called: {}", mirror); //todo: switch to debug!
     let app_state = app_handle.state::<AppState>();
     let mut settings = app_state
         .settings
@@ -226,7 +226,7 @@ fn set_idf_mirror(app_handle: AppHandle, mirror: &str) {
         })
         .expect("Failed to lock settings");
     (*settings).idf_mirror = Some(mirror.to_string());
-    println!("Setting after idf_mirror: {:?}", settings); //todo: switch to debug!
+    log::debug!("Setting after idf_mirror: {:?}", settings); //todo: switch to debug!
 }
 
 #[tauri::command]
@@ -235,7 +235,7 @@ fn get_tools_mirror_list() -> &'static [&'static str] {
 }
 #[tauri::command]
 fn set_tools_mirror(app_handle: AppHandle, mirror: &str) {
-    println!("set_tools_mirror called: {}", mirror); //todo: switch to debug!
+    log::info!("set_tools_mirror called: {}", mirror); //todo: switch to debug!
     let app_state = app_handle.state::<AppState>();
     let mut settings = app_state
         .settings
@@ -249,7 +249,7 @@ fn set_tools_mirror(app_handle: AppHandle, mirror: &str) {
         })
         .expect("Failed to lock settings");
     (*settings).mirror = Some(mirror.to_string());
-    println!("Setting after tools_mirror: {:?}", settings); //todo: switch to debug!
+    log::debug!("Setting after tools_mirror: {:?}", settings); //todo: switch to debug!
 }
 
 #[tauri::command]
@@ -282,8 +282,7 @@ fn load_settings(app_handle: AppHandle, path: &str) {
             )
         })
         .expect("Failed to parse TOML");
-    println!("settings {:?}", settings); // TODO: remove debug print statement
-                                         // TODO: load setting to the app state
+    log::debug!("settings {:?}", settings);
     let app_state = app_handle.state::<AppState>();
     let mut state_settings = app_state
         .settings
@@ -321,6 +320,19 @@ use tauri::{AppHandle, Emitter};
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_log::Builder::new()
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Stdout,
+                ))
+                .target(tauri_plugin_log::Target::new(
+                    tauri_plugin_log::TargetKind::Webview,
+                ))
+                .level(log::LevelFilter::Error)
+                .level_for("idf_im_lib", log::LevelFilter::Info)
+                .level_for("eim_lib", log::LevelFilter::Info)
+                .build(),
+        )
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
             let app_state = AppState::default();
