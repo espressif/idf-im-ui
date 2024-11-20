25eca7014feeb5d62c52183532970bcfdebfaa37
Petr Gadorek <petr.gadorek@espressif.com>
2024-11-20 13:31:02 +0100

    added version to the footer

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     <global-progress />
     <!-- Footer -->
     <footer class="footer">
-      ESP-IDF Installation Manager v1.0.0 (TODO:INSERT VERSION)
+      ESP-IDF Installation Manager {{ appVersion }}
     </footer>
   </div>
 </template>
@@ -17,14 +17,19 @@
 <script setup>
 import { NConfigProvider, NLayout, NLayoutHeader, NLayoutContent, useOsTheme } from 'naive-ui'
 import { darkTheme } from 'naive-ui'
-import { computed, onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import MessageDisplay from './components/MessageDisplay.vue'
 import GlobalProgress from './components/GlobalProgress.vue'
 import { attachConsole } from '@tauri-apps/plugin-log'
+import { getVersion } from '@tauri-apps/api/app';
+
+
 const osTheme = useOsTheme()
 const theme = null // computed(() => (osTheme.value === 'dark' ? darkTheme : null))
+const appVersion = ref('');
 
 onMounted(async () => {
-  const detach = await attachConsole()
+  const detach = await attachConsole();
+  appVersion.value = await getVersion();
 })
 </script>
