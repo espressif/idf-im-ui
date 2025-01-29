e843130c1de56dde5729690e6981bf23a6527746
Petr Gadorek <petr.gadorek@espressif.com>
2025-01-29 07:30:55 +0100

    fixed navigation

--- src/components/LogLink.vue
+++ src/components/LogLink.vue
@@ -14,7 +14,8 @@ export default {
     this.LogPath = await invoke("get_logs_folder", {});
   },
   methods: {
-    async open_logs() {
+    async open_logs(e) {
+      e.preventDefault();
       console.log("Opening logs folder: " + this.LogPath);
       invoke("show_in_folder", { path: this.LogPath });
       console.log("Logs folder opened.");
@@ -24,6 +25,6 @@ export default {
 </script>
 <template>
   <p>
-    <a href="#" @click="open_logs($event)">Logs folder</a>
+    <a href="_self" @click="open_logs($event)">Logs folder</a>
   </p>
 </template>
