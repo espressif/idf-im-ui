ba743740ef849954608fb87a54d7cd161f0261c1
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2025-02-24 09:53:59 +0000

    [EIM-125] [Test] refactor automated tests (#55)
    * Test refactor -> simplify launch and customization
    * update workflow

--- tests/scripts/defaultInstall.test.js
+++ tests/scripts/defaultInstall.test.js
@@ -1,70 +1,65 @@
-import os from "os";
-import path from "path";
 import { expect } from "chai";
-import { describe, it, before, after, beforeEach, afterEach } from "mocha";
+import { describe, it, before, after, afterEach } from "mocha";
 import { EIMRunner } from "../classes/tauriRunner.class.js";
 import logger from "../classes/logger.class.js";
 
-let pathToEim;
+export function runInstallSimplified(id, pathToEIM) {
+    let eimRunner = "";
 
-if (process.env.EIM_GUI_PATH) {
-    pathToEim = process.env.EIM_GUI_PATH;
-} else {
-    pathToEim =
-        os.platform() !== "win32"
-            ? path.resolve(os.homedir(), "eim-gui", "eim")
-            : path.resolve(os.homedir(), "eim-gui", "eim.exe");
-}
+    describe("1- EIM Application Launch", () => {
+        let simplifiedInstallFailed = false;
 
-let eimRunner = "";
+        before(async function () {
+            this.timeout(30000);
+            eimRunner = new EIMRunner(pathToEIM);
+            try {
+                await eimRunner.launchEIM();
+            } catch (err) {
+                logger.info("Error starting EIM application");
+            }
+        });
 
-describe("EIM Application Launch", () => {
-    before(async function () {
-        this.timeout(30000);
-        eimRunner = new EIMRunner(pathToEim);
-        try {
-            await eimRunner.launchEIM();
-        } catch (err) {
-            logger.info("Error starting EIM application");
-        }
-    });
+        beforeEach(async function () {
+            if (simplifiedInstallFailed) {
+                logger.info("Test failed, skipping next tests");
+                this.skip();
+            }
+        });
 
-    afterEach(async function () {
-        if (this.currentTest.state === "failed") {
-            await eimRunner.takeScreenshot(`${this.currentTest.title}.png`);
-            logger.info(`Screenshot saved as ${this.currentTest.title}.png`);
-        }
-    });
+        afterEach(async function () {
+            if (this.currentTest.state === "failed") {
+                await eimRunner.takeScreenshot(
+                    `${id} ${this.currentTest.title}.png`
+                );
+                logger.info(
+                    `Screenshot saved as ${id} ${this.currentTest.title}.png`
+                );
+                simplifiedInstallFailed = true;
+            }
+        });
 
-    after(async function () {
-        this.timeout(5000);
-        try {
-            await eimRunner.closeEIM();
-        } catch (error) {
-            logger.info("Error to close IEM application");
-        }
-    });
+        after(async function () {
+            this.timeout(5000);
+            try {
+                await eimRunner.closeEIM();
+            } catch (error) {
+                logger.info("Error to close IEM application");
+            }
+        });
 
-    it("Should show welcome page", async function () {
-        this.timeout(10000);
-
-        try {
+        it("1- Should show welcome page", async function () {
+            this.timeout(10000);
             // Wait for the header to be present
             const header = await eimRunner.findByCSS("h1");
             const text = await header.getText();
             expect(text, "Expected welcome text").to.equal(
                 "Welcome to ESP-IDF Installation Manager!"
             );
-        } catch (error) {
-            logger.info("Failed to get Welcome header", error);
-            throw error;
-        }
-    });
+        });
 
-    it("Should show installation options", async function () {
-        this.timeout(10000);
+        it("2- Should show installation options", async function () {
+            this.timeout(10000);
 
-        try {
             await eimRunner.clickButton("Get Started");
             const header = await eimRunner.findByDataId("main-title");
             const text = await header.getText();
@@ -78,16 +73,10 @@ describe("EIM Application Launch", () => {
                 await simplified.isDisplayed(),
                 "Expected option for simplified installation"
             ).to.be.true;
-        } catch (error) {
-            logger.info("Failed to locate get started button", error);
-            throw error;
-        }
-    });
+        });
 
-    it("Should install IDF using simplified setup", async function () {
-        this.timeout(1300000);
-
-        try {
+        it("3- Should install IDF using simplified setup", async function () {
+            this.timeout(1330000);
             await eimRunner.clickButton("Start Simplified Setup");
             await new Promise((resolve) => setTimeout(resolve, 5000));
             const installing = await eimRunner.findByText(
@@ -100,7 +89,7 @@ describe("EIM Application Launch", () => {
             ).to.be.true;
             const startTime = Date.now();
 
-            while (Date.now() - startTime < 1200000) {
+            while (Date.now() - startTime < 1300000) {
                 if (await eimRunner.findByText("Installation Failed", 1000)) {
                     logger.debug("failed!!!!");
                     break;
@@ -122,16 +111,10 @@ describe("EIM Application Launch", () => {
                 await completed.isDisplayed(),
                 "Expected 'Installation Complete' text displayed"
             ).to.be.true;
-        } catch (error) {
-            logger.info("Failed to complete installation", error);
-            throw error;
-        }
-    });
+        });
 
-    it("Should offer to save installation configuration", async function () {
-        this.timeout(1300000);
-
-        try {
+        it("4- Should offer to save installation configuration", async function () {
+            this.timeout(10000);
             const saveConfig = await eimRunner.findByText("Save Configuration");
             expect(saveConfig, "Expected screen for saving configuration").to
                 .not.be.false;
@@ -141,9 +124,6 @@ describe("EIM Application Launch", () => {
             ).to.be.true;
             const exit = await eimRunner.findByText("Exit Installer");
             expect(exit, "Expected option to exit installer").to.not.be.false;
-        } catch (error) {
-            logger.info("Failed to complete installation", error);
-            throw error;
-        }
+        });
     });
-});
+}
