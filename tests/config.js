d6ea857db36b16c7f2f2b94ef8b3be73191c93e2
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2025-04-21 17:33:26 +0100

    Enable Extended and mirror + fix artifact location

--- tests/config.js
+++ tests/config.js
@@ -1,19 +1,19 @@
 const IDFVERSIONSSHOWNONINSTALLER = [
-    `master`,
-    `v5.4.1`,
-    `v5.3.2`,
-    `v5.2.5`,
-    `v5.1.6`,
+  `master`,
+  `v5.4.1`,
+  `v5.3.3`,
+  `v5.2.5`,
+  `v5.1.6`,
 ];
 
 const IDFMIRRORS = {
-    github: "https://github.com",
-    jihulab: "https://jihulab.com/esp-mirror",
+  github: "https://github.com",
+  jihulab: "https://jihulab.com/esp-mirror",
 };
 const TOOLSMIRRORS = {
-    github: "https://github.com",
-    dl_com: "https://dl.espressif.com/github_assets",
-    dl_cn: "https://dl.espressif.cn/github_assets",
+  github: "https://github.com",
+  dl_com: "https://dl.espressif.com/github_assets",
+  dl_cn: "https://dl.espressif.cn/github_assets",
 };
 
 const CLIDEFAULTVERSION = "eim 0.2.0";
@@ -22,10 +22,10 @@ const GUIDEFAULTVERSION = "0.2.0";
 const IDFDEFAULTINSTALLVERSION = "v5.4.1";
 
 export {
-    IDFVERSIONSSHOWNONINSTALLER,
-    IDFMIRRORS,
-    TOOLSMIRRORS,
-    CLIDEFAULTVERSION,
-    GUIDEFAULTVERSION,
-    IDFDEFAULTINSTALLVERSION,
+  IDFVERSIONSSHOWNONINSTALLER,
+  IDFMIRRORS,
+  TOOLSMIRRORS,
+  CLIDEFAULTVERSION,
+  GUIDEFAULTVERSION,
+  IDFDEFAULTINSTALLVERSION,
 };
