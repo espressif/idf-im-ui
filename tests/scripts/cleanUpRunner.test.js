134755b101c11b8d9adb08edcabae71e8588d579
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2025-08-11 14:34:24 +0100

    Updated tests to set cleanup task optional.

---
+++ tests/scripts/cleanUpRunner.test.js
@@ -0,0 +1,32 @@
+import { expect } from "chai";
+import { describe, it, after } from "mocha";
+import logger from "../classes/logger.class.js";
+import fs from "fs";
+
+export function runCleanUp({ installFolder, toolsFolder, deleteAfterTest }) {
+  describe("4- Clean up EIM folders from Runner ->", function () {
+    after(function () {
+      if (deleteAfterTest) {
+        logger.info("Running clean up function");
+        try {
+          fs.rmSync(installFolder, { recursive: true, force: true });
+          logger.info(`Successfully deleted ${installFolder} folder`);
+          fs.rmSync(toolsFolder, {
+            recursive: true,
+            force: true,
+          });
+          logger.info(`Successfully deleted ${toolsFolder} folder`);
+        } catch (err) {
+          logger.info(`Error deleting installation folders`);
+        }
+      }
+    });
+
+    it("1. Install and Tools folder should exist", async function () {
+      logger.info("Validating folders exist before deleting");
+      expect(fs.existsSync(installFolder), "IDF Installation folder missing").to
+        .be.true;
+      expect(fs.existsSync(toolsFolder), "IDF tools folder missing").to.be.true;
+    });
+  });
+}
