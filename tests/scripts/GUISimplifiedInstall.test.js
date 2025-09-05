2b3c1c595d1f81a304b1e54399e6bcd43533587f
Fabricio Ribeiro <fabricio.ribeiro@espressif.com>
2025-09-05 14:19:36 +0100

    Fix Startup and default install test on GUI

--- tests/scripts/GUISimplifiedInstall.test.js
+++ tests/scripts/GUISimplifiedInstall.test.js
@@ -44,25 +44,27 @@ export function runGUISimplifiedInstallTest(id, pathToEIM) {
     });
 
     it("1- Should show welcome page", async function () {
-      this.timeout(10000);
+      this.timeout(25000);
       // Wait for the header to be present
+      await new Promise((resolve) => setTimeout(resolve, 10000));
       const header = await eimRunner.findByCSS("h1");
       const text = await header.getText();
       expect(text, "Expected welcome text").to.equal(
-        "Welcome to ESP-IDF Installation Manager!"
+        "Welcome to ESP-IDF Installation Manager"
       );
     });
 
     it("2- Should show installation options", async function () {
       this.timeout(10000);
 
-      await eimRunner.clickButton("Get Started");
-      const header = await eimRunner.findByDataId("main-title");
+      await eimRunner.clickButton("Start Installation");
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const header = await eimRunner.findByCSS("h1");
       const text = await header.getText();
       expect(text, "Expected installation setup screen").to.equal(
-        "Installation Setup"
+        "Install ESP-IDF"
       );
-      const simplified = await eimRunner.findByText("Simplified Installation");
+      const simplified = await eimRunner.findByText("Easy Installation");
       expect(simplified, "Expected option for simplified installation").to.not
         .be.false;
       expect(
@@ -71,17 +73,36 @@ export function runGUISimplifiedInstallTest(id, pathToEIM) {
       ).to.be.true;
     });
 
-    it("3- Should install IDF using simplified setup", async function () {
+    it("3- Should show installation summary", async function () {
+      this.timeout(25000);
+
+      await eimRunner.clickButton("Start Easy Installation");
+      await new Promise((resolve) => setTimeout(resolve, 15000));
+      const header = await eimRunner.findByCSS("h2");
+      const text = await header.getText();
+      expect(text, "Expected installation summary screen").to.equal(
+        "Ready to Install"
+      );
+      const startButton = await eimRunner.findByText("Start Installation");
+      expect(startButton, "Expected button to start installation").to.not.be
+        .false;
+      expect(
+        await startButton.isDisplayed(),
+        "Expected start button to be displayed"
+      ).to.be.true;
+    });
+
+    it("4- Should install IDF using simplified setup", async function () {
       this.timeout(2730000);
-      await eimRunner.clickButton("Start Simplified Setup");
+      await eimRunner.clickButton("Start Installation");
       await new Promise((resolve) => setTimeout(resolve, 5000));
       const installing = await eimRunner.findByText(
-        "Installing ESP-IDF...",
+        "Downloading ESP-IDF",
         20000
       );
 
-      expect(installing, "Expected installation progress screen").to.not.be
-        .false;
+      expect(installing, "Expected installation to start Downloading ESP-IDF")
+        .to.not.be.false;
       expect(
         await installing.isDisplayed(),
         "Expected installation progress screen"
@@ -111,17 +132,24 @@ export function runGUISimplifiedInstallTest(id, pathToEIM) {
       ).to.be.true;
     });
 
-    it("4- Should offer to save installation configuration", async function () {
+    it("5- Should show installation complete summary", async function () {
       this.timeout(10000);
-      const saveConfig = await eimRunner.findByText("Save Configuration");
-      expect(saveConfig, "Expected screen for saving configuration").to.not.be
-        .false;
+      const documentationButton = await eimRunner.findByText(
+        "View Documentation"
+      );
+      expect(documentationButton, "Expected button to show documentation").to
+        .not.be.false;
       expect(
-        await saveConfig.isDisplayed(),
-        "Expected option to save configuration to be displayed"
+        await documentationButton.isDisplayed(),
+        "Expected Expected button to show documentation to be displayed"
       ).to.be.true;
-      const exit = await eimRunner.findByText("Exit Installer");
-      expect(exit, "Expected option to exit installer").to.not.be.false;
+      const dashboardButton = await eimRunner.findByText("Go to Dashboard");
+      expect(dashboardButton, "Expected button to return to dashboard").to.not
+        .be.false;
+      expect(
+        await dashboardButton.isDisplayed(),
+        "Expected Expected button to return to dashboard to be displayed"
+      ).to.be.true;
     });
   });
 }
