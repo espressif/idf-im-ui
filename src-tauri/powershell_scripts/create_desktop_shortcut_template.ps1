aff84404a09a4a1062d24fd84817a7da3493348c
Hahihula <hahihula@gmail.com>
2026-03-26 10:41:44 +0100

    EIM-645: resolve onedrive desktop bug (#630)

--- src-tauri/powershell_scripts/create_desktop_shortcut_template.ps1
+++ src-tauri/powershell_scripts/create_desktop_shortcut_template.ps1
@@ -1,10 +1,11 @@
 $iconPath = "$env:USERPROFILE\Icons\eim.ico"
+$desktop = [Environment]::GetFolderPath("Desktop")
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$env:USERPROFILE\Desktop\IDF_{{name}}_Powershell.lnk")
+$Shortcut = $WshShell.CreateShortcut("$desktop\IDF_{{name}}_Powershell.lnk")
 $Shortcut.TargetPath = "powershell.exe"
 $Shortcut.Arguments = "-NoExit -ExecutionPolicy Bypass -NoProfile -Command `"& {. '{{custom_profile_filename}}'}`""
-$Shortcut.WorkingDirectory = "$env:USERPROFILE\Desktop"
+$Shortcut.WorkingDirectory = $desktop
 $Shortcut.IconLocation = $iconPath
 $Shortcut.Save()
 
-Write-Host "Shortcut created on the desktop: IDF_Powershell.lnk" -ForegroundColor Green
+Write-Host "Shortcut created on the desktop: IDF_{{name}}_Powershell.lnk" -ForegroundColor Green
